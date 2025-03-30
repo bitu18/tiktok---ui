f950095cca56ba58100da488d21fae824e7140c8
bitu <tungocanh173@gmail.com>
2025-03-30 00:45:57 -0500

    update

--- src/layouts/components/Search/Search.js
+++ src/layouts/components/Search/Search.js
@@ -5,7 +5,7 @@ import classNames from 'classnames/bind';
 import { useEffect, useRef, useState } from 'react';
 import AccountItem from '~/component/AccountItem';
 import { Wrapper as PopperWrapper } from '~/component/Popper';
-import { SearchIcon } from '~/component/Icons';
+// import { SearchIcon } from '~/component/Icons';
 import { useDebounce } from '~/hooks';
 import styles from './Search.module.scss';
 import * as searchServices from '~/serivces/searchService';
