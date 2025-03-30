86c463683d4f2fdf15b60fe15ff3dcc2c9ac66a2
bitu <tungocanh173@gmail.com>
2025-03-30 00:11:28 -0500

    update

--- src/layouts/components/Header/Header.js
+++ src/layouts/components/Header/Header.js
@@ -15,13 +15,13 @@ import {
     HelpIcon,
     ShortcutsIcon,
     LogoutIcon,
-} from 'src/component/Icons';
+} from '~/component/Icons';
 
 import styles from './Header.module.scss';
 import images from '~/assets/images';
 import Button from '~/component/Button';
 import Menu from '~/component/Popper/Menu';
-import Image from 'src/component/Image';
+import Image from '~/component/Image';
 import Search from '../Search';
 import config from '~/config';
 // import { useForm } from '~/';
