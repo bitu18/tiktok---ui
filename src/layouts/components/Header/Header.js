d15429630d6c55c8fed1899b84da76f8416fc999
bitu <tungocanh173@gmail.com>
2025-03-30 00:00:43 -0500

    update

--- src/layouts/components/Header/Header.js
+++ src/layouts/components/Header/Header.js
@@ -21,7 +21,7 @@ import styles from './Header.module.scss';
 import images from '~/assets/images';
 import Button from '~/component/Button';
 import Menu from '~/component/Popper/Menu';
-import Image from '../../../component/Image';
+import Image from 'src/component/Image';
 import Search from '../Search';
 import config from '~/config';
 // import { useForm } from '~/';
