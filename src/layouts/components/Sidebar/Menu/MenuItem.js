403dfe6ef7378b86a5c71cf48ee51908152dd6c5
Bitu <tungocanh173@gmail.com>
2025-05-19 14:45:29 -0500

    update

--- src/layouts/components/Sidebar/Menu/MenuItem.js
+++ src/layouts/components/Sidebar/Menu/MenuItem.js
@@ -7,12 +7,8 @@ const cx = classNames.bind(styles);
 function MenuItem({ title, to, icon }) {
     return (
         <NavLink className={(nav) => cx('menu-item', { active: nav.isActive })} to={to}>
-            {({ isActive }) => (
-                <>
-                    {icon}
-                    <span className={cx('title')}>{title}</span>
-                </>
-            )}
+            <span className={cx('icon')}>{icon}</span>
+            <span className={cx('title')}>{title}</span>
         </NavLink>
     );
 }
--- src/layouts/components/Sidebar/Sidebar.js
+++ src/layouts/components/Sidebar/Sidebar.js
@@ -1,43 +1,116 @@
 import classNames from 'classnames/bind';
 import styles from './Sidebar.module.scss';
 import Menu, { MenuItem } from './Menu';
-// import {
-//     HomeIcon,
-//     HomeIconActive,
-//     UserGroupIcon,
-//     UserGroupIconActive,
-//     LiveIcon,
-//     LiveIconActive,
-// } from '~/component/Icons';
 import config from '~/config';
 import Button from '~/component/Button';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faPlay, faPlus, faStore } from '@fortawesome/free-solid-svg-icons';
+import {
+    faChalkboardUser,
+    faClockRotateLeft,
+    faGamepad,
+    faListUl,
+    faPlay,
+    faPlus,
+    faSchool,
+    faStore,
+} from '@fortawesome/free-solid-svg-icons';
 // import { faNoteSticky, faPenToSquare, faUser, faPenToSquare } from '@fortawesome/free-regular-svg-icons';
-import { faNoteSticky, faUsers, faPenToSquare } from '@fortawesome/free-solid-svg-icons';
+import { faPenToSquare } from '@fortawesome/free-solid-svg-icons';
+import { faCompass, faFaceSmile, faFileLines, faFolderOpen } from '@fortawesome/free-regular-svg-icons';
+import { useState } from 'react';
+
+const STUDENT_ITEMS = [
+    {
+        icon: <FontAwesomeIcon icon={faPenToSquare} />,
+        title: 'Create Quest',
+        to: '/create',
+    },
+    {
+        icon: <FontAwesomeIcon icon={faCompass} />,
+        title: 'Explore Quests',
+        to: '/explore',
+    },
+    {
+        icon: <FontAwesomeIcon icon={faListUl} />,
+        title: 'My Quests',
+        to: '/worksheet',
+    },
+    {
+        icon: <FontAwesomeIcon icon={faFaceSmile} />,
+        title: 'Characters',
+        to: '/character',
+    },
+    {
+        icon: <FontAwesomeIcon icon={faFolderOpen} />,
+        title: 'Worksheets',
+        to: '/worksheet',
+    },
+    {
+        icon: <FontAwesomeIcon icon={faClockRotateLeft} />,
+        title: 'Player Log',
+        to: '/playerLog',
+    },
+    {
+        icon: <FontAwesomeIcon icon={faStore} />,
+        title: 'Merchant',
+        to: '/market',
+    },
+    {
+        icon: <FontAwesomeIcon icon={faGamepad} />,
+        title: 'Classroom games',
+        to: '/game',
+    },
+];
+
+const TEACHER_ITEMS = [
+    {
+        icon: <FontAwesomeIcon icon={faListUl} />,
+        title: 'My Quests',
+        to: '/worksheet',
+    },
+    {
+        icon: <FontAwesomeIcon icon={faChalkboardUser} />,
+        title: 'My Classes',
+        to: '/classes',
+    },
+    {
+        icon: <FontAwesomeIcon icon={faFileLines} />,
+        title: 'Homework',
+        to: '/homework',
+    },
+    {
+        icon: <FontAwesomeIcon icon={faFolderOpen} />,
+        title: 'Worksheets',
+        to: '/worksheet',
+    },
+    {
+        icon: <FontAwesomeIcon icon={faGamepad} />,
+        title: 'Classroom games',
+        to: '/game',
+    },
+];
 
 const cx = classNames.bind(styles);
 
 function Sidebar() {
+    const [studentUser, setStudentUser] = useState(true);
+
     return (
         <aside className={cx('wrapper')}>
-            <Button primary large leftIcon={<FontAwesomeIcon icon={faPlay} />} className={cx('btn')}>
-                Play
+            <Button
+                primary
+                large
+                leftIcon={<FontAwesomeIcon icon={studentUser ? faPlay : faPlus} />}
+                className={cx('btn')}
+            >
+                {studentUser ? 'Play' : 'Create'}
             </Button>
-            <Menu>
-                <MenuItem
-                    title="Worksheets"
-                    to={config.routes.worksheet}
-                    icon={<FontAwesomeIcon icon={faNoteSticky} />}
-                />
-                {/* <MenuItem title="About us" to={config.routes.live} icon={<FontAwesomeIcon icon={faUsers} />} /> */}
-                <MenuItem title="Create" to={config.routes.create} icon={<FontAwesomeIcon icon={faPenToSquare} />} />
-                <MenuItem title="Merchant" to={config.routes.market} icon={<FontAwesomeIcon icon={faStore} />} />
-            </Menu>
 
-            {/* 
-            <SuggestedAccounts label="Suggested Accounts" />
-            <SuggestedAccounts label="Following Accounts" /> */}
+            <Menu>
+                {(studentUser ? STUDENT_ITEMS : TEACHER_ITEMS).map((item, index) => (
+                    <MenuItem key={index} title={item.title} to={item.to} icon={item.icon} />
+                ))}
+            </Menu>
 
             <Button primary small leftIcon={<FontAwesomeIcon icon={faPlus} />} className={cx('btn--upgrade')}>
                 Upgrade to Epic Tier
